667f758140274cc0c7189065c7cf49ef706f74e7
Yash Zade <yashmzade@gmail.com>
2024-11-11 16:12:14 +0530

    removed web config

--- src/main/java/com/project/taxibookingapp/controllers/AuthController.java
+++ src/main/java/com/project/taxibookingapp/controllers/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path="/auth")
 @RequiredArgsConstructor
