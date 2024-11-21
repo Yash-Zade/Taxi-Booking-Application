a3e5205d6aa9b53b2e80097f5ed5d6dcc41201f9
Yash Zade <yashmzade@gmail.com>
2024-11-21 12:53:50 +0530

    swager configration

--- src/main/java/com/project/taxibookingapp/configs/SwaggerConfig.java
+++ src/main/java/com/project/taxibookingapp/configs/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +20,10 @@ public class SwaggerConfig {
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
-                .info(new Info().title("CabZilla API").version("1.0").description("API documentation for CabZilla"));
+                .info(new Info().title("CabZilla API").version("1.0").description("API documentation for CabZilla"))
+                .servers(List.of(
+                        new Server().url("https://localhost:8080").description("Local server"),
+                        new Server().url("https://cabzilla.up.railway.app").description("Production server")
+                ));
     }
 }
